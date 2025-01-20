078bf4ceaf2330385d7c5a37425ecc854f5f08b2
SomneelSaha2004 <somneel.saha27@gmail.com>
2025-01-20 15:41:38 +0800

    Level-1

---
+++ src/main/java/TaskMaster.java
@@ -0,0 +1,11 @@
+public class TaskMaster {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________"+
+        "\nHello! I'm Taskmaster" +
+        "\nWhat can I do for you?"+
+                "\n____________________________________________________________"+
+        "\nBye. Hope to see you again soon!"+
+                "\n____________________________________________________________";
+        System.out.println(logo);
+    }
+}
