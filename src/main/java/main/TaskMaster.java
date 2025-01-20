4028350b0aeec80b66d9a09b92e4c263d7ecff3a
SomneelSaha2004 <somneel.saha27@gmail.com>
2025-01-20 16:46:31 +0800

    add Level 3

--- src/main/java/main/TaskMaster.java
+++ src/main/java/main/TaskMaster.java
@@ -14,40 +14,47 @@ public class TaskMaster {
         System.out.println(logo);
         Scanner sc =  new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
-        
+
+        label:
         while(true){
             System.out.print("> ");
             String task = sc.nextLine();
             String[] s = task.split(" ");
             String command = s[0].trim();
-            if (command.equals("bye")){
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if(command.equals("list")){
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++){
-                    
-                    System.out.println(i+1 + ". " + tasks.get(i).toString());
+            switch (command) {
+                case "bye":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                    break label;
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+
+                        System.out.println(i + 1 + ". " + tasks.get(i).toString());
+                    }
+                    break;
+                case "mark": {
+                    int index = Integer.parseInt(s[1].trim());
+                    tasks.get(index - 1).markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks.get(index - 1).toString());
+                    break;
                 }
-            } else if(command.equals("mark")){
-                int index =  Integer.parseInt(s[1].trim());
-                tasks.get(index-1).markAsDone();
-                System.out.println("____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks.get(index-1).toString());
-            }else if(command.equals("unmark")){
-                int index =  Integer.parseInt(s[1].trim());
-                tasks.get(index-1).markAsNotDone();
-                System.out.println("____________________________________________________________");
-                System.out.println("Ok, I've marked this task as not done yet:");
-                System.out.println(tasks.get(index-1).toString());
-            }
-            else{
-            tasks.add(new Task(task));
-            System.out.println("added: "+task);
+                case "unmark": {
+                    int index = Integer.parseInt(s[1].trim());
+                    tasks.get(index - 1).markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                    System.out.println(tasks.get(index - 1).toString());
+                    break;
+                }
+                default:
+                    tasks.add(new Task(task));
+                    System.out.println("added: " + task);
+                    break;
             }
             System.out.println("____________________________________________________________");
         }
