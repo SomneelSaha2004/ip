55e10611e578abbf2d62ae9db0bb6db7ab7ed775
SomneelSaha2004 <somneel.saha27@gmail.com>
2025-01-20 15:42:01 +0800

    Level-1

--- src/main/java/TaskMaster.java
+++ src/main/java/TaskMaster.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class TaskMaster {
     public static void main(String[] args) {
         String logo = "____________________________________________________________"+
         "\nHello! I'm Taskmaster" +
         "\nWhat can I do for you?"+
-                "\n____________________________________________________________"+
-        "\nBye. Hope to see you again soon!"+
                 "\n____________________________________________________________";
         System.out.println(logo);
+        Scanner sc =  new Scanner(System.in);
+        while(true){
+            System.out.print("> ");
+            String command = sc.nextLine();
+            if (command.trim().equals("bye")){
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("    "+command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
