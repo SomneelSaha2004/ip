1b726a5a23827e6496320bb8502a8b1611c4eaf9
SomneelSaha2004 <somneel.saha27@gmail.com>
2025-01-20 15:47:35 +0800

    Level 2 Add,List

--- src/main/java/TaskMaster.java
+++ src/main/java/TaskMaster.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class TaskMaster {
     public static void main(String[] args) {
         String logo = "____________________________________________________________"+
@@ -7,16 +8,25 @@ public class TaskMaster {
                 "\n____________________________________________________________";
         System.out.println(logo);
         Scanner sc =  new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
         while(true){
             System.out.print("> ");
             String command = sc.nextLine();
+            
             if (command.trim().equals("bye")){
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if(command.trim().equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++){
+                    System.out.println(i+1 + ". " + tasks.get(i));
+                }
+            } else{
+            tasks.add(command.trim());      
+            System.out.println("added: "+command);
             }
-            System.out.println("    "+command);
             System.out.println("____________________________________________________________");
         }
     }
