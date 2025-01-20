6cbd8f54d03a33f1b7aca50b71c17ee9b5f5dc50
SomneelSaha2004 <somneel.saha27@gmail.com>
2025-01-21 01:20:21 +0800

    UI testing

--- src/main/java/main/TaskMaster.java
+++ src/main/java/main/TaskMaster.java
@@ -1,6 +1,7 @@
 package main;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class TaskMaster {
     public static void main(String[] args) {
         String logo = """
@@ -12,73 +13,101 @@ public class TaskMaster {
                 
                 ____________________________________________________________""";
         System.out.println(logo);
-        Scanner sc =  new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
 
         label:
-        while(true){
+        while (true) {
             System.out.print("> ");
-            String task = sc.nextLine();
-            String[] s = task.split(" ");
-            String command = s[0].trim();
+            String userInput = sc.nextLine().trim();
+
+
+            String[] parts = userInput.split(" ", 2);
+            String command = parts[0].trim();
+
+            String remainder = (parts.length > 1) ? parts[1].trim() : "";
+
             switch (command) {
                 case "bye":
                     System.out.println("____________________________________________________________");
                     System.out.println("Bye. Hope to see you again soon!");
                     System.out.println("____________________________________________________________");
                     break label;
+
                 case "list":
                     System.out.println("____________________________________________________________");
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
-
-                        System.out.println(i + 1 + ". " + tasks.get(i).toString());
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
                     break;
+
                 case "mark": {
-                    int index = Integer.parseInt(s[1].trim());
+
+                    int index = Integer.parseInt(remainder);
                     tasks.get(index - 1).markAsDone();
                     System.out.println("____________________________________________________________");
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks.get(index - 1).toString());
+                    System.out.println(tasks.get(index - 1));
                     break;
                 }
+
                 case "unmark": {
-                    int index = Integer.parseInt(s[1].trim());
+
+                    int index = Integer.parseInt(remainder);
                     tasks.get(index - 1).markAsNotDone();
                     System.out.println("____________________________________________________________");
                     System.out.println("Ok, I've marked this task as not done yet:");
-                    System.out.println(tasks.get(index - 1).toString());
+                    System.out.println(tasks.get(index - 1));
                     break;
                 }
+
                 case "todo":
-                    tasks.add(new ToDo(task));
+
+                    tasks.add(new ToDo(remainder));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println("  "+tasks.get(tasks.size() - 1).toString());
-                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    System.out.println("  " + tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
+
                 case "deadline":
-                    String[] deadline = task.split("/by");
-                    tasks.add(new Deadline(deadline[0].substring(8), deadline[1].trim()));
+
+                    String[] deadlineParts = remainder.split("/by", 2);
+                    String deadlineDesc = deadlineParts[0].trim();
+                    String deadlineBy = (deadlineParts.length > 1) ? deadlineParts[1].trim() : "";
+                    tasks.add(new Deadline(deadlineDesc, deadlineBy));
+
                     System.out.println("Got it. I've added this task:");
-                    System.out.println("  "+tasks.get(tasks.size() - 1).toString());
-                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    System.out.println("  " + tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
+
                 case "event":
 
-                    String[] event =  task.split("/");
-                    String from  = event[1].split(" ")[1]+" "+event[1].split(" ")[2];
-                    String to  = event[2].split(" ")[1];
-                    tasks.add(new Event(event[0],from,to));
+                    String[] fromSplit = remainder.split("/from", 2);
+                    String eventDesc = fromSplit[0].trim();
+                    String fromAndTo = (fromSplit.length > 1) ? fromSplit[1].trim() : "";
+
+
+                    String[] toSplit = fromAndTo.split("/to", 2);
+                    String fromTime = toSplit[0].trim();
+                    String toTime = (toSplit.length > 1) ? toSplit[1].trim() : "";
+
+                    tasks.add(new Event(eventDesc, fromTime, toTime));
+
                     System.out.println("Got it. I've added this task:");
-                    System.out.println("  "+tasks.get(tasks.size() - 1).toString());
-                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    System.out.println("  " + tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     break;
+
                 default:
-                    tasks.add(new Task(task));
-                    System.out.println("added: " + task);
+
+                    tasks.add(new Task(userInput));
+                    System.out.println("added: " + userInput);
                     break;
             }
+
             System.out.println("____________________________________________________________");
         }
     }
